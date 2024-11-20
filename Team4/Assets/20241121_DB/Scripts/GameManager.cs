cee21385fa0678e90ee028f8c8d24d2f6652e4b5
Jin <voltagevault@gmail.com>
2024-11-20 11:35:48 +0900

    Temp

--- Team4/Assets/20241121_DB/Scripts/GameManager.cs
+++ Team4/Assets/20241121_DB/Scripts/GameManager.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    private List<GameObject> itemList = new List<GameObject>();
+    private List<GameObject> itemList = new List<GameObject>(24);
 
     private GameObject go;
     private Vector3 goPos;
-    private float dist;
+    private Vector3 dist = new Vector3(8f, 0f, 0f);
 
     private Vector3 mousePos;
+
     private void Update()
     {
         BuildObject();
@@ -24,20 +25,23 @@ public class GameManager : MonoBehaviour
         RaycastHit hit;
 
         // 원래 오브젝트로부터의 임의 거리 설정
-        dist = 3f;
 
         if (Input.GetMouseButtonDown(0))
         {
             Physics.Raycast(ray, out hit);
-            
-            // 빈공간 클릭했을 때 Null Exception 해줄 것
+
+            if (hit.collider == null)
+            {
+                return;
+            }
+
             go = hit.transform.gameObject;
-            goPos = go.transform.position * dist;
-            
+            goPos = go.transform.position + dist;
+
             if (go.CompareTag("StoreItems"))
             {
                 GameObject newGo =
-                    Instantiate(go, goPos, Quaternion.identity);
+                    Instantiate(go, goPos, Quaternion.identity, go.transform);
                 itemList.Add(newGo);
                 newGo.tag = "InventoryItems";
             }
