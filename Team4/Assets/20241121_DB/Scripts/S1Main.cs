d3b3beb8de8f2bdb624ed7c46af5f54f85b8e2d9
MINGUK\h1865 <gkalsrnr12@naver.com>
2024-11-20 16:53:55 +0900

    no message

--- Team4/Assets/20241121_DB/Scripts/S1Main.cs
+++ Team4/Assets/20241121_DB/Scripts/S1Main.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+using UnityEngine.Networking;
 
 public class S1Main : MonoBehaviour
 {
@@ -18,17 +21,48 @@ public class S1Main : MonoBehaviour
     private GameObject joinUI = null;
     [SerializeField]
     private GameObject exitUI = null;
-    
-    
+    [SerializeField]
+    private TMP_InputField username;
+    [SerializeField]
+    private TMP_InputField password;
+
+    private const string loginURL = "http://127.0.0.1/login.php";
+
 
     private void Start()
     {
-        Loginbt.onClick.AddListener(() => OnLogin());
+        Loginbt.onClick.AddListener(() => StartCoroutine(LoginCoroutine(username.text, password.text)));
         joinbt.onClick.AddListener(() => OnjoinUI(true));
         exitbt.onClick.AddListener(() => OnExitUI(true));
         Jclosebt.onClick.AddListener(() => Jclose(false));
         Eclosebt.onClick.AddListener(() => Eclose(false));
     }
+    private IEnumerator LoginCoroutine(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(loginURL, form))
+        {
+            yield return www.SendWebRequest();
+
+            string response = www.downloadHandler.text;
+            Debug.Log("서버 응답: " + response);
+
+            if (response.Contains("Login success"))
+            {
+                Debug.Log("로그인 성공!");
+                SceneManager.LoadScene("Scene2"); // 다음 씬으로 전환
+            }
+            else
+            {
+                Debug.Log("로그인 실패: " + response);
+            }
+        }
+    }
+
+
     private void OnLogin()
     {
         SceneManager.LoadScene("Scene2");
